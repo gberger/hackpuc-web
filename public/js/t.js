019e4bdd3011812025b3c7f931ab1ab0b962b544
Guilherme Berger <guilherme.berger@gmail.com>
2015-12-12 08:24:34 -0200

    set up google maps

--- public/js/t.js
+++ public/js/t.js
@@ -7,3 +7,12 @@ socket.emit('subscribe', { room: trackingId });
 socket.on('status', function(status) {
   console.log(status);
 });
+
+var myLatlng = new google.maps.LatLng(-34.397, 150.644);
+var mapOptions = {
+  zoom: 8,
+  center: myLatlng,
+  mapTypeId: google.maps.MapTypeId.SATELLITE
+};
+var map = new google.maps.Map(document.getElementById("map"),
+  mapOptions);
