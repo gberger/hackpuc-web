9da4ce9c4bde08cd3f1de2b32641f2eed08607b2
Guilherme Berger <guilherme.berger@gmail.com>
2015-12-12 11:29:22 -0200

    track status on map

--- config/io.js
+++ config/io.js
@@ -1,8 +1,28 @@
 'use strict';
 
+var _ = require('lodash');
+var mongoose = require('mongoose');
+var Alert = mongoose.model('Alert');
+var Firing = mongoose.model('Firing');
+var Status = mongoose.model('Status');
+
 module.exports = function(io) {
   io.sockets.on('connection', function (socket) {
-    socket.on('subscribe', function(data) { socket.join(data.room); });
-    socket.on('unsubscribe', function(data) { socket.leave(data.room); });
+    socket.on('subscribe', function(data) {
+      Firing.findById(data.room, function(err, firing) {
+        socket.join(data.room);
+
+        Status.find({firing: firing}).sort({timestamp: 'ascending'}).exec(function(err, statuses) {
+          if (err) console.log(err);
+          console.log(statuses);
+          for(var status of statuses) {
+            socket.emit('status', status)
+          }
+        });
+      });
+    });
+    socket.on('unsubscribe', function(data) {
+      socket.leave(data.room);
+    });
   });
 };
--- public/js/t.js
+++ public/js/t.js
@@ -1,18 +1,52 @@
 "use strict";
 
-var socket = io.connect();
+var myLatlng = new google.maps.LatLng(-22.97982535792792, -43.23314334269914);
+var mapOptions = {
+  zoom: 13,
+  center: myLatlng,
+  mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-socket.emit('subscribe', { room: trackingId });
 
-socket.on('status', function(status) {
-  console.log(status);
+var poly = new google.maps.Polyline({
+  map: map,
+  strokeColor: '#FF0000',
+  strokeOpacity: 1.0,
+  strokeWeight: 2
 });
 
-var myLatlng = new google.maps.LatLng(-34.397, 150.644);
-var mapOptions = {
-  zoom: 8,
-  center: myLatlng,
-  mapTypeId: google.maps.MapTypeId.SATELLITE
-};
-var map = new google.maps.Map(document.getElementById("map"),
-  mapOptions);
+
+
+var socket = io.connect();
+var initialMarker, finalMarker;
+var initialInfoWindow = new google.maps.InfoWindow({content: "Localização inicial."});
+var finalInfoWindow = new google.maps.InfoWindow({content: "Última localização."});
+socket.on('status', function(status) {
+  status.date = new Date(status.timestamp);
+  poly.getPath().push(new google.maps.LatLng(status.latitude, status.longitude));
+
+  if (poly.getPath().length == 1) {
+    initialMarker = new google.maps.Marker({
+      map: map,
+      position: new google.maps.LatLng(status.latitude, status.longitude)
+    });
+    initialInfoWindow.setContent("Localização inicial: " + status.latitude + ", " + status.longitude + " em " + status.date);
+    initialMarker.addListener('click', function() {
+      initialInfoWindow.open(map, initialMarker);
+    });
+  } else if (poly.getPath().length == 2) {
+    finalMarker = new google.maps.Marker({
+      map: map,
+      position: new google.maps.LatLng(status.latitude, status.longitude)
+    });
+    finalMarker.addListener('click', function() {
+      finalInfoWindow.open(map, finalMarker);
+    });
+  } else {
+    finalInfoWindow.setContent("Localização inicial: " + status.latitude + ", " + status.longitude + " em " + status.date);
+    finalMarker.setPosition(new google.maps.LatLng(status.latitude, status.longitude));
+  }
+});
+
+socket.emit('subscribe', { room: trackingId });
